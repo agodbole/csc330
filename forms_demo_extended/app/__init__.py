9b6ffe9149e940222505a63c702f93503815f3f4
amol <agodbole@gmail.com>
2025-02-24 14:05:22 +0000

    Added code to extend forms demo to use an array instead of a disctionary

---
+++ forms_demo_extended/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csc330 spring 2025'
+
+# dictionary of cities with their population
+# cities = {}
+cities = []
+
+from app import routes
+
